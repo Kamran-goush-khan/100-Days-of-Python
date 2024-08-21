51e72bbbbdd90de83ee8e1a51e18b23dcb4cfef7
Kamran-goush-khan <100296501+Kamran-goush-khan@users.noreply.github.com>
2024-08-21 14:22:31 +0530

    Day 6- other are reborg world task

---
+++ Day 6- Function, Code Block and While loops/Function.py
@@ -0,0 +1,12 @@
+def my_function(name) :
+    print(f"Hello, {name}")
+
+def addition(num1, num2) :
+    my_function("Kamran")
+    return num1 + num2
+
+
+my_function("Angela")
+
+print(addition(4, 5))
+
